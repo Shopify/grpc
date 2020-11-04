02ef887f469a76c3caaae3092340bb59840352e1
Nicolas Laurent <nicolas.laurent@oracle.com>
2020-11-04 16:54:08 +0100

    add RbConfig's cppflag to CPPFLAGS when using TruffleRuby
    this ensure the paths to find the system's OpenSSL are set up correctly with TruffleRuby (includes being able to find an OpenSSL installed via Homebrew etc)
    reb

--- src/ruby/ext/grpc/extconf.rb
+++ src/ruby/ext/grpc/extconf.rb
@@ -52,6 +52,11 @@ inherit_rbconfig 'LDFLAGS'
 ENV['LD'] = ENV['CC'] if env_unset?('LD')
 ENV['LDXX'] = ENV['CXX'] if env_unset?('LDXX')
 
+if RUBY_ENGINE == 'truffleruby'
+  # ensure we can find the system's OpenSSL
+  env_append 'CPPFLAGS', RbConfig::CONFIG['cppflags']
+end
+
 def apple_toolchain?
   # TruffleRuby uses the Sulong LLVM runtime, which is different from Apple's.
   RUBY_PLATFORM =~ /darwin/ && RUBY_ENGINE != 'truffleruby'
@@ -77,7 +82,7 @@ if apple_toolchain?
   end
 end
 
-ENV['CPPFLAGS'] = '-DGPR_BACKWARDS_COMPATIBILITY_MODE'
+env_append 'CPPFLAGS', '-DGPR_BACKWARDS_COMPATIBILITY_MODE'
 
 output_dir = File.expand_path(RbConfig::CONFIG['topdir'])
 grpc_lib_dir = File.join(output_dir, 'libs', grpc_config)
